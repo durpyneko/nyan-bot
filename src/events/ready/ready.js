51474fcb71fa82c73c34dcd9c15cf3264704e110
durpyneko <89787577+iDurpyDude12@users.noreply.github.com>
2023-11-15 13:47:32 +0200

    set bot presence

--- src/events/ready/ready.js
+++ src/events/ready/ready.js
@@ -1,6 +1,12 @@
 // Requirements
 const logger = require("../../utils/logger");
 
+// Require the necessary discord.js classes
+const { ActivityType } = require("discord.js");
+
+// Define the command prefix
+const { prefix } = require("../../config.json");
+
 // Main
 module.exports = (client) => {
   // Logging
@@ -8,5 +14,16 @@ module.exports = (client) => {
     name: "ready",
     type: "info",
     content: `${client.user.username} is online!`,
+  });
+
+  // Set presence
+  client.user.setPresence({
+    activities: [
+      {
+        name: `for sauce. ${prefix}help`,
+        type: ActivityType.Watching,
+      },
+    ],
+    status: "online",
   });
 };
