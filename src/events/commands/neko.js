2f01b160b85350a3835730dd1903165ea47cb072
durpyneko <89787577+iDurpyDude12@users.noreply.github.com>
2023-11-15 13:47:19 +0200

    small change: use embed and image link instead

--- src/events/commands/neko.js
+++ src/events/commands/neko.js
@@ -11,11 +11,28 @@ module.exports = {
       // Extract the neko image URL from the response
       const imageUrl = response.data.neko;
 
+      const nekoEmbed = {
+        tts: false,
+        embeds: [
+          {
+            type: "rich",
+            title: "Here is a neko for you:",
+            description: "",
+            color: 0xdf3079,
+            image: {
+              url: `${imageUrl}`,
+              height: 0,
+              width: 0,
+            },
+          },
+        ],
+      };
+
       // Reply to the message with the neko image
-      message.reply({
-        content: "Here is a neko for you:",
-        files: [imageUrl],
-      });
+      message.reply(
+        nekoEmbed
+        //files: [imageUrl],
+      );
     } catch (error) {
       console.error(error);
       message.reply("Failed to fetch a neko :(");
